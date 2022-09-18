30a329056da8d9fd48624ca280d8239078593afd
Nils Emmenegger <61921611+gingers-have-souls@users.noreply.github.com>
2022-09-17 20:07:09 -0400

    Render id in room's page

--- src/frontend/src/components/Rooms.js
+++ src/frontend/src/components/Rooms.js
@@ -23,6 +23,7 @@ export default function Rooms(props) {
         <>
             <p>Name: {roomData.name}</p>
             <p>Location: {roomData.location}</p>
+            <p>ID: {roomData.id}</p>
             <QRCode value={"http://127.0.0.1:8000/room/"+id} />
         </>
     );
