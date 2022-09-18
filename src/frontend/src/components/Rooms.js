fc68f67c0a27be0a66aeadba11b5f6de96e7fe62
Nils Emmenegger <61921611+gingers-have-souls@users.noreply.github.com>
2022-09-17 21:20:24 -0400

    Add "Add Project" button to rooms page"

--- src/frontend/src/components/Rooms.js
+++ src/frontend/src/components/Rooms.js
@@ -6,9 +6,10 @@ import './rooms.css'
 export default function Rooms(props) {
     const { id } = useParams()
     const [roomData, setRoomData] = useState({})
+    const [showAddProjectMenu, setShowAddProjectMenu] = useState(false)
 
-    function getRoomData(roomNumber){
-        fetch("/api/room/"+ roomNumber)
+    function getRoomData(roomNumber) {
+        fetch("/api/room/" + roomNumber)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
@@ -18,7 +19,7 @@ export default function Rooms(props) {
 
     useEffect(() => {
         getRoomData(id);
-    }, []);
+    }, [])
 
     return (
         <div id='roompage'>
@@ -27,17 +28,33 @@ export default function Rooms(props) {
                     <p>Name: {roomData.name}</p>
                     <p>Location: {roomData.location}</p>
                     <p>ID: {roomData.id}</p>
-                    <QRCode value={"http://127.0.0.1:8000/room/"+id} />
+                    <QRCode value={"http://127.0.0.1:8000/room/" + id} />
+                    {showAddProjectMenu ?
+                        <>
+                            <h2>List of commits</h2>
+                        </>
+                        :
+                        <>
+                            <button onClick={() => { setShowAddProjectMenu(true) }}>Add Project</button>
+                            <br/>
+                            <input
+                                id='addProjectInput'
+                                type='text'
+                                name='Add Project'
+                                placeholder='Project name (e.g. owenmoogk/github-rooms)'
+                            />
+                        </>
+                    }
                 </>
-                : roomData.failure ? 
-                <>
-                    <h1>Github Rooms</h1>
-                    <br/>
-                    <p>Room does not exist :(</p>
-                    <p>Redirecting you to the homepage.</p>
-                    <div style={{display: 'none'}}>{setTimeout(() => window.location.href = "http://127.0.0.1:8000/", 3000)}</div>
-                </>
-                : null
+                    : roomData.failure ?
+                        <>
+                            <h1>Github Rooms</h1>
+                            <br />
+                            <p>Room does not exist :(</p>
+                            <p>Redirecting you to the homepage.</p>
+                            <div style={{ display: 'none' }}>{setTimeout(() => window.location.href = "http://127.0.0.1:8000/", 3000)}</div>
+                        </>
+                        : null
             }
         </div>
     );
