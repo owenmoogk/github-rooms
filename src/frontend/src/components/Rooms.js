53dce753de860d1f427681fe9039b0c1e4ebe3c7
Nils Emmenegger <61921611+gingers-have-souls@users.noreply.github.com>
2022-09-17 20:04:49 -0400

    Show name and location on a room's page

--- src/frontend/src/components/Rooms.js
+++ src/frontend/src/components/Rooms.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import QRCode from "react-qr-code";
 
 export default function Rooms(props) {
-    const { id } = useParams();
+    const { id } = useParams()
+    const [roomData, setRoomData] = useState({})
 
-    function getRoom(roomNumber){
+    function getRoomData(roomNumber){
         fetch("/api/room/"+ roomNumber)
             .then((response) => response.json())
-            .then((data) => console.log(data))
+            .then((data) => {
+                console.log(data)
+                setRoomData(data)
+            })
     }
+
+    useEffect(() => {
+        getRoomData(id);
+    }, []);
 
     return (
         <>
+            <p>Name: {roomData.name}</p>
+            <p>Location: {roomData.location}</p>
             <QRCode value={"http://127.0.0.1:8000/room/"+id} />
-            <button onClick={() => getRoom(2)}> get room</button>
-            <button onClick={() => makeNewRoom()}>make room</button>
         </>
     );
 }
