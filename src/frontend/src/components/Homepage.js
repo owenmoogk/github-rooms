8fde6bf0eb12fed9abea09781e14a338edd18090
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-09-17 20:05:20 -0400

    homepage

--- src/frontend/src/components/Homepage.js
+++ src/frontend/src/components/Homepage.js
@@ -7,7 +7,8 @@ export default function Homepage(){
     <div className='homepage'>
       <h1>Github Rooms</h1>
       <p className='subtitle'>A software dev's LinkedIn.</p>
-      <input placeholder='Room # (ex. 123456)'></input>
+      <input id='roomInput' placeholder='Room # (ex. 123456)' type='number'></input>
+      <button onClick={() => window.location.href = "http://127.0.0.1:8000/" + document.getElementById('roomInput').value}>Go!</button>
       <p>Or, scan the barcode!</p>
       <a id='addBarcode'></a>
     </div>
